158012056c83bf10291dd2314ab794738dc2b63a
maik <081210040@faculdade.cefsa.edu.br>
2024-09-30 22:13:36 -0300

    Iniciando

---
+++ api/conta.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var config = require('config');
+var router = express.Router();
+
+// routes
+router.get('/', about);
+
+module.exports = router;
+
+
+function range(start, stop, step) {
+    if (typeof stop == 'undefined') {
+        // one param defined
+        stop = start;
+        start = 0;
+    }
+
+    if (typeof step == 'undefined') {
+        step = 1;
+    }
+
+    if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+        return [];
+    }
+
+    var result = [];
+    for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+        result.push(i);
+    }
+
+    return result;
+};
+
+function calcular_x(i) {
+    return i + (1 / i)
+}
+
+function calcular_recursivo(m, n) {
+    i = m
+
+    if (i <= n) {
+        resultado = calcular_x(i) * calcular_recursivo(i)
+    } else   {
+        resultado = resultado * calcular_x(i)
+    }
+
+    return resultado
+    
+}
+
+async function about(req, res) {
+   
+    req_m = req.query.m
+    req_n = req.query.n
+    req_opcao = req.query.opcao
+
+    resultado = 0
+    
+    if (req_opcao == "iterativa") {
+        resultado = 1
+        limites = range( parseInt(req_m), parseFloat(req_n) + 1)
+        limites.forEach(i => {
+            resultado = resultado * calcular_x(i)
+        });
+    } else if (req_opcao == "recursiva") {
+        resultado = calcular_recursivo(parseInt(req_m), parseFloat(req_n))
+    }
+    
+
+   res.send(
+    String(resultado)
+   );
+}
